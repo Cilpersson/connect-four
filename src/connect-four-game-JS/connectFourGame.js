22aa567aa380400e16fc177cc094402152225138
Christina Persson <c.i.l.persson@gmail.com>
2020-09-09 17:06:54 +0200

    I think I might have managed to setup the test for the pure js file

--- src/connect-four-game-JS/connectFourGame.js
+++ src/connect-four-game-JS/connectFourGame.js
@@ -10,4 +10,17 @@ export class ConnectFour {
     ];
     this.currentPlayer = 1;
   }
+
+  dropDisc(index) {
+    // Needs to check the arrays at same index to see how low it can be dropped
+
+    for (let i = 0; i < 6; i++) {
+      if (this.gameBoard[0][index] === "") {
+      }
+    }
+  }
+
+  checkForFourInARow() {
+    // This method should be called inside of dropDisc()
+  }
 }
