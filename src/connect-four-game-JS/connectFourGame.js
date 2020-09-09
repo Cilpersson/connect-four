a8b5b371fcd2230ab6f31ab2ce76ce0920f53b3a
Christina Persson <c.i.l.persson@gmail.com>
2020-09-09 21:11:18 +0200

    Working on tthe diagonal method in the connect four game class

--- src/connect-four-game-JS/connectFourGame.js
+++ src/connect-four-game-JS/connectFourGame.js
@@ -1,47 +1,77 @@
 export class ConnectFour {
   constructor() {
-    this.gameBoard = [
-      // ["", "", "", "", "", ""],
-      // ["", "", "", "", "", ""],
-      // ["", "", "", "", "", ""],
-      // ["", "", "", "", "", ""],
-      // ["", "", "", "", "", ""],
-      // ["", "", "", "", "", ""],
-      // ["", "", "", "", "", ""],
-      [],
-      [],
-      [],
-      [],
-      [],
-      [],
-      [],
-    ];
+    this.gameBoard = [[], [], [], [], [], [], []];
     this.currentPlayer = 1;
     this.winner = 0;
   }
 
-  dropDisc(currentPlayer, index) {
-    if (this.gameBoard[index].length < 6) {
-      this.gameBoard[index].push(currentPlayer);
+  dropDisc(currentPlayer, verticalIndex) {
+    if (this.gameBoard[verticalIndex].length < 6 && this.winner === 0) {
+      this.gameBoard[verticalIndex].push(currentPlayer);
     }
-
     //Every time a disc has been dropped check for winner should be called
-    // checkForWinner(index)
+    let horizontalIndex = this.gameBoard[verticalIndex].length - 1;
+    this.checkForWinner(currentPlayer, verticalIndex, horizontalIndex);
   }
 
-  checkForWinner() {
-    if (this.isFourInARowDiagonal(player)) return true;
-    if (this.isFourInARowVertical(player)) return true;
-    if (this.isFourInARowHorizontal(player)) return true;
+  checkForWinner(player, verticalIndex, horizontalIndex) {
+    if (
+      this.isFourInARowVertical(player, verticalIndex) ||
+      this.isFourInARowHorizontal(player, horizontalIndex) ||
+      this.isFourInARowDiagonal(player, verticalIndex, horizontalIndex)
+    ) {
+      this.winner = player;
+      return true;
+    }
   }
-  isFourInARowDiagonal(player) {}
+  isFourInARowDiagonal(player, verticalIndex, horizontalIndex) {
+    // METHOD 1 FIND TOP AND BOTTOM LEFT AND GO RIGHT DIAGONALLY
+    // STEP 1: Define outer edge indexes and move from current place untill you hit the index
+    // Remember top left and bottom left index
+    // STEP 2: Start moving from top left diagonally down and count if 4 return true
+    // STEP 3: Start moving from bottom left and count if 4 return true
 
-  isFourInARowHorizontal(player) {}
+    let leftMostIndex = 0;
+    let bottomMostIndex = 0;
+    let topMostIndex = 5;
+    let rightMostIndex = 6;
 
-  isFourInARowVertical(player, index) {
+    // IF VERTICAL INDEX > HORIZONTAL GO BACK HORIZONTAL INDEX
+    // IF VERTICAL INDEX < HORIZONTAL GO BACK VERTICAL INDEX
+
+    let downAndLeftSteps =
+      verticalIndex >= horizontalIndex ? horizontalIndex : verticalIndex;
+
+    let upAndLeftSteps =
+      verticalIndex >= topMostIndex - horizontalIndex
+        ? topMostIndex - horizontalIndex
+        : verticalIndex;
+
+    //METHOD 2 Move down left and top right from current untill you "Miss"
+    // Remember count and add, if >= 4 return true
+    //
+  }
+
+  isFourInARowHorizontal(player, horizontalIndex) {
+    let counter = 0;
+    let isFourInARow = false;
+
+    this.gameBoard.forEach((arr) => {
+      if (arr[horizontalIndex] === player) {
+        counter++;
+        // Counter can't be returned because it keeps counting after four in a row is found
+        counter === 4 ? (isFourInARow = true) : (isFourInARow = false);
+      } else {
+        counter = 0;
+      }
+    });
+    return isFourInARow;
+  }
+
+  isFourInARowVertical(player, verticalIndex) {
     let counter = 0;
 
-    this.gameBoard[index].forEach((value) => {
+    this.gameBoard[verticalIndex].forEach((value) => {
       if (value === player) {
         counter++;
       } else {
--- src/connect-four-game-JS/connectFourGame.spec.js
+++ src/connect-four-game-JS/connectFourGame.spec.js
@@ -43,12 +43,51 @@ describe("You should not be able to add more than six discs to one row", () => {
   });
 });
 
-describe("For discs from the same player in a vertical row results in winning the game", () => {
+describe("For discs from the same player in a vertical line results in winning the game", () => {
   it("should return true if player one puts for discs on top of each other", () => {
     game.dropDisc(1, 3);
     game.dropDisc(1, 3);
     game.dropDisc(1, 3);
     game.dropDisc(1, 3);
     expect(game.isFourInARowVertical(1, 3)).toEqual(true);
+  });
+});
+
+describe("Four discs from the same player in a vertical line should not result in winning game if the other player has a disc inbetween", () => {
+  it("should return true if player one puts for discs on top of each other", () => {
+    game.dropDisc(1, 3);
+    game.dropDisc(1, 3);
+    game.dropDisc(-1, 3);
+    game.dropDisc(1, 3);
+    game.dropDisc(1, 3);
+    expect(game.isFourInARowVertical(1, 3)).toEqual(false);
+  });
+});
+
+describe("A disc should not be able to be placed after a player has won", () => {
+  it("should ", () => {
+    game.dropDisc(1, 3);
+    game.dropDisc(1, 3);
+    game.dropDisc(1, 3);
+    game.dropDisc(1, 3);
+    game.dropDisc(1, 3);
+    expect(game.gameBoard).toEqual([[], [], [], [1, 1, 1, 1], [], [], []]);
+  });
+});
+
+describe("For discs from the same player in a horizontal line results in winning the game", () => {
+  it("should return true if player one puts for discs on next to each other", () => {
+    game.dropDisc(1, 1);
+    game.dropDisc(1, 2);
+    game.dropDisc(1, 3);
+    game.dropDisc(1, 4);
+    expect(game.isFourInARowHorizontal(1, 0)).toEqual(true);
+  });
+});
+
+describe("For discs from the same player in a diagonal line results in winning the game", () => {
+  it("should return true if player one puts for discs diagonally next to each other", () => {
+    game.gameBoard = [[1], [-1, 1], [-1, -1, 1], [-1, -1, -1, 1], [], [], []];
+    expect(game.isFourInARowDiagonal(1, 2, 2)).toEqual(true);
   });
 });
